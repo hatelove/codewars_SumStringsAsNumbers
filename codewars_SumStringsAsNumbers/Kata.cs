6d72cc0aeb9c7e6003a8769f74c2212f8e6782f2
hatelove <jay.hatelove@gmail.com>
2017-08-29 22:07:46 +0800

    add the first failed test case: a is 1 and b is 2, should return 3

---
+++ codewars_SumStringsAsNumbers/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codewars_SumStringsAsNumbers
+{
+    class Kata
+    {
+        public static int sumStrings(string a, string b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ codewars_SumStringsAsNumbers/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace codewars_SumStringsAsNumbers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void string_1_and_2_sum_Should_be_3()
+        {
+            var a = "1";
+            var b = "2";
+            var expected = 3;
+            int actual = Kata.sumStrings(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
