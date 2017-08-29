7cef8c8946e19173e0ee9d7a6c7244705ed494db
hatelove <jay.hatelove@gmail.com>
2017-08-29 22:59:38 +0800

    add a failed test case: "24" + "3" should be "27"

--- codewars_SumStringsAsNumbers/UnitTest1.cs
+++ codewars_SumStringsAsNumbers/UnitTest1.cs
@@ -14,7 +14,13 @@ namespace codewars_SumStringsAsNumbers
         [TestMethod]
         public void string_4_and_8_should_be_12()
         {
-            SumShouldBe("4","8","12");
+            SumShouldBe("4", "8", "12");
+        }
+
+        [TestMethod]
+        public void string_24_and_3_should_be_27()
+        {
+            SumShouldBe("24", "3", "27");
         }
 
         private static void SumShouldBe(string a, string b, string expected)
